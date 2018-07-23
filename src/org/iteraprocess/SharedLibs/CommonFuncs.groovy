2b4131fba7dd58c745026f5f1711d74fc2ae15aa
Pedro Valero <pvaleror@gmail.com>
2018-07-23 11:19:13 -0500

    Shared Script

--- src/org/iteraprocess/SharedLibs/CommonFuncs.groovy
+++ src/org/iteraprocess/SharedLibs/CommonFuncs.groovy
@@ -1,13 +1,9 @@
 package org.iteraprocess.SharedLibs;
 
-
-def Error(string) {
-  echo "\u001B[31m ----- " + string + " ++++++ \u001B[0m"
+class CommonFuncs implements Serializable{
+  def steps
+  Utilities(steps) {this.steps = steps}
+  def Error(string) {
+    echo "\u001B[31m ----- " + string + " ++++++ \u001B[0m"
+  }
 }
-  
-return this  
-//class CommonFuncs implements Serializable{
-//  def steps
-//  Utilities(steps) {this.steps = steps}
-//  
-//}
