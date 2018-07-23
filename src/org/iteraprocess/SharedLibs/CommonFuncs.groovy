f350db469fdb1c02f69d74255d6db3204a624206
Pedro Valero <pvaleror@gmail.com>
2018-07-23 14:56:18 -0500

    Shared Script

--- src/org/iteraprocess/SharedLibs/CommonFuncs.groovy
+++ src/org/iteraprocess/SharedLibs/CommonFuncs.groovy
@@ -5,25 +5,4 @@ class CommonFuncs implements Serializable{
   CommonFuncs(steps) {
     this.steps = steps
   }
-  def log(msg) {
-    spit(msg, '\033[34m')
-  }
-
-  def error(msg) {
-    spit(msg, '\033[31m')
-  }
-
-  def warning(msg){
-    spit(msg, '\033[33m')
-  }
-
-  def success(msg){
-    spit(msg, '\033[32m')
-  }
-
-  private def spit(msg, color){
-    ansiColor('xterm') {
-      steps.echo color + msg + '\033[0m'
-    }
-  }
 }
