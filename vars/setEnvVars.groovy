de41fea1b437b174158ef22d703bb9f5c35155e6
Pedro Valero <pvaleror@gmail.com>
2018-07-23 13:44:21 -0500

    Shared Script

--- vars/setEnvVars.groovy
+++ vars/setEnvVars.groovy
@@ -1,7 +1,7 @@
 def call(configFiles) {
   for(int i=0; i < configFiles.size(); i++){
     def cnfName = configFiles[i]
-    configFileProvider([configFile(fileId: cnfName, variable: tmp)]){
+    configFileProvider([configFile(fileId: cnfName, variable: 'tmp')]){
       script{
         def shProps = sh(returnStdout: true, script: 'cat '+tmp).trim()
         def props = readProperties text: shProps, replaceTokens: true;
