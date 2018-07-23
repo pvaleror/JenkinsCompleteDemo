8a6ae09b67a7ae980a69d61e8d92fa5498bc9d46
Pedro Valero <pvaleror@gmail.com>
2018-07-23 14:44:07 -0500

    Shared Script

--- vars/Console.groovy
+++ vars/Console.groovy
@@ -3,13 +3,13 @@ def debug(String msg){
   echo '\033[34m' + msg + '\033[0m'
 }
 
-def call(Map conf) {
-  switch(conf.type){
-    case 'debug':
-      debug(conf.msg)
-      break;
-    default:
-      debug(conf.msg)
-      break;
-  }
-}
+//def call(Map conf) {
+//  switch(conf.type){
+//    case 'debug':
+//      debug(conf.msg)
+//      break;
+//    default:
+//      debug(conf.msg)
+//      break;
+//  }
+//}
