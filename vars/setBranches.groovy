ca8977eb7e1af1484b9803afdca9880f3c27beb9
Pedro Valero <pvaleror@gmail.com>
2018-08-09 13:03:33 -0500

    Set branches

--- vars/setBranches.groovy
+++ vars/setBranches.groovy
@@ -2,10 +2,10 @@ def call(Map config){
   if(env.TARGET == "Development"){
     def sourceBranch = sh(returnStdout: true, script: 'git config --get gitflow.prefix.feature').trim() + ${params.ID_RECORD}
     def targetBranch = sh(returnStdout: true, script: 'git config --get gitflow.branch.develop').trim()
-  }elsif($enviroment eq "_CLIENTES"){
+  }elseif(env.TARGET == "Test"){
     def sourceBranch = sh(returnStdout: true, script: 'git config --get gitflow.prefix.feature').trim() + ${params.ID_RECORD};
     def targetBranch = sh(returnStdout: true, script: 'git config --get gitflow.prefix.release').trim() + ${params.ID_RECORD};
-  }elsif($enviroment eq "_PRODUCCION"){
+  }elseif(env.TARGET == "Production"){
     def sourceBranch = sh(returnStdout: true, script: 'git config --get gitflow.prefix.release').trim() + ${params.ID_RECORD};
     def targetBranch = sh(returnStdout: true, script: 'git config --get gitflow.branch.master').trim()
   }
