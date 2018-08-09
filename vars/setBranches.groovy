cec7718e20b00f28cb161a6055ac3a29a5c2650b
Pedro Valero <pvaleror@gmail.com>
2018-08-09 14:44:28 -0500

    Set branches

--- vars/setBranches.groovy
+++ vars/setBranches.groovy
@@ -1,13 +1,15 @@
 def call(Map config){
+  def sourceBranch
+  def targetBranch
   if(env.TARGET == "Development"){
-    def sourceBranch = sh(returnStdout: true, script: 'git config --get gitflow.prefix.feature').trim() + params.ID_RECORD
-    def targetBranch = sh(returnStdout: true, script: 'git config --get gitflow.branch.develop').trim()
+    sourceBranch = sh(returnStdout: true, script: 'git config --get gitflow.prefix.feature').trim() + params.ID_RECORD
+    targetBranch = sh(returnStdout: true, script: 'git config --get gitflow.branch.develop').trim()
   } else if(env.TARGET == "Test"){
-    def sourceBranch = sh(returnStdout: true, script: 'git config --get gitflow.prefix.feature').trim() + params.ID_RECORD
-    def targetBranch = sh(returnStdout: true, script: 'git config --get gitflow.prefix.release').trim() + params.ID_RECORD
+    sourceBranch = sh(returnStdout: true, script: 'git config --get gitflow.prefix.feature').trim() + params.ID_RECORD
+    targetBranch = sh(returnStdout: true, script: 'git config --get gitflow.prefix.release').trim() + params.ID_RECORD
   } else if(env.TARGET == "Production"){
-    def sourceBranch = sh(returnStdout: true, script: 'git config --get gitflow.prefix.release').trim() + params.ID_RECORD
-    def targetBranch = sh(returnStdout: true, script: 'git config --get gitflow.branch.master').trim()
+    sourceBranch = sh(returnStdout: true, script: 'git config --get gitflow.prefix.release').trim() + params.ID_RECORD
+    targetBranch = sh(returnStdout: true, script: 'git config --get gitflow.branch.master').trim()
   }
   env['sourceBranch'] = sourceBranch
   env['targetBranch'] = targetBranch
